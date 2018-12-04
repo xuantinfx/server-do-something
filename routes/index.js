eed3649f31554c647e10a7e66df4391d2f6c3d53
Xuân Tin <xuantinfx@gmail.com>
2018-12-04 23:17:37 +0700

    init

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+const mailler = require('../lib/mailler')
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+module.exports = router;
---
+++ routes/postInfRouter.js
@@ -0,0 +1,25 @@
+var express = require('express');
+const mailler = require('../lib/mailler')
+var router = express.Router();
+
+router.post('/', function (req, res, next) {
+    res.setHeader('content-type', "text/json; charset=utf-8")
+
+    // Xử lí data và gửi mail
+    let data = req.body.data;
+    if(data.macdiff < -1.15) {
+
+        mailler.sendMail('index', {data: JSON.stringify(req.body.data)}, 'xuantinfx@gmail.com', "Hello From Gekko")
+            .then(info => {
+                console.log('send mail success', info)
+            })
+            .catch(err => {
+                console.log('send mail error', info)
+            })
+    }
+    res.end(JSON.stringify({
+        status: 'success'
+    }))
+});
+
+module.exports = router;
